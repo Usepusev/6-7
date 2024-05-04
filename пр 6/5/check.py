d6363f58f91dbc4004401f261b17d6c3c05ff614
Usepusev <gerasimovv39@gmail.com>
2024-05-04 21:39:34 +0300

    6-5

--- пр 6/5/check.py
+++ пр 6/5/check.py
@@ -6,19 +6,18 @@ def check(a, b, c, bl):
         c = float(c)
         if a+b>c and a+c>b and b+c>a:
             bl = True
-            res = ""
             return [a, b, c, bl]
         else:
-            res = "Такого треугольника не существует!"
+            print("Такого треугольника не существует!")
             a = ""
             b = ""
             c = ""
             bl = False
-            return [a, b, c, bl, res]
+            return [a, b, c, bl]
     except ValueError:
-        res = "Вы ввели что-то не то!"
+        print("Вы ввели что-то не то!")
         a = ""
         b = ""
         c = ""
         bl = False
-        return [a, b, c, bl, res]
+        return [a, b, c, bl]
